dd821a571d145acbd79263a0c5dc25db35234ea3
Arkadiusz Sekuła <arkadiusz.sekula@comarch.pl>
2019-09-03 15:07:24 +0200

    * testing generating service from schematic ok

--- ang-io-example/src/app/arq-arq.service.ts
+++ ang-io-example/src/app/arq-arq.service.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class <%= classify(name) %>Service {
+export class ArqArqService {
   constructor(private http: HttpClient) {
   }
 }
