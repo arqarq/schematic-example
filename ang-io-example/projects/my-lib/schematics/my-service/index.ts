73d34ba902f3d397eadfb88b3b268bb1c17d3821
Arkadiusz Sekuła <arkadiusz.sekula@comarch.pl>
2019-09-03 14:10:46 +0200

    * added workspace ang-io-example with lib with schematic; * improved scripts in 'package.json' to work in Windows

---
+++ ang-io-example/projects/my-lib/schematics/my-service/index.ts
@@ -0,0 +1,44 @@
+import {
+  apply,
+  applyTemplates,
+  chain,
+  mergeWith,
+  move,
+  Rule,
+  SchematicsException,
+  Tree,
+  url
+} from '@angular-devkit/schematics';
+import { experimental, normalize, strings } from '@angular-devkit/core';
+import { Schema as MyServiceSchema } from './schema';
+
+export function myService(options: MyServiceSchema): Rule {
+  return (tree: Tree) => {
+    const workspaceConfig = tree.read('/angular.json');
+    if (!workspaceConfig) {
+      throw new SchematicsException('Could not find Angular workspace configuration');
+    }
+    const workspaceContent = workspaceConfig.toString();
+    const workspace: experimental.workspace.WorkspaceSchema = JSON.parse(workspaceContent);
+    if (!options.project) {
+      options.project = workspace.defaultProject;
+    }
+    const projectName = options.project as string;
+    const project = workspace.projects[projectName];
+    const projectType = project.projectType === 'application' ? 'app' : 'lib';
+    if (options.path === undefined) {
+      options.path = `${project.sourceRoot}/${projectType}`;
+    }
+    const templateSource = apply(url('./files'), [
+      applyTemplates({
+        classify: strings.classify,
+        dasherize: strings.dasherize,
+        name: options.name
+      }),
+      move(normalize(options.path as string))
+    ]);
+    return chain([
+      mergeWith(templateSource)
+    ]);
+  };
+}
